432286ebfd37cb9b25fc00ad92e7447431655c62
Jeremiah Wodke <jeremiah.wodke@gmail.com>
2019-12-22 13:56:02 -0800

    relationships added

--- app/Models/Listing.js
+++ app/Models/Listing.js
@@ -4,6 +4,15 @@
 const Model = use('Model')
 
 class Listing extends Model {
+    /** @type {typeof import('./ListingFile')} */
+    files() {
+        return this.hasMany('App/Models/ListingFile')
+    }
+
+    /** @type {typeof import('./ListingImage')} */
+    images() {
+        return this.hasMany('App/Models/ListingFile')
+    }
 }
 
 module.exports = Listing
--- app/Models/User.js
+++ app/Models/User.js
@@ -4,6 +4,14 @@
 const Model = use('Model')
 
 class User extends Model {
+    static get hidden() {
+        return ['password']
+    }
+
+    /** @type {typeof import('./Listing')} */
+    listings() {
+        return this.hasMany('App/Models/Listing')
+    }
 }
 
 module.exports = User
